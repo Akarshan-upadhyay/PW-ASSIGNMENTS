41adf3bb14c62a56c6d7656dbd6a9895785a79d6
Akarshan-upadhyay <121966381+Akarshan-upadhyay@users.noreply.github.com>
2023-01-06 16:17:57 +0530

    Update Q10.java

--- Java Loops/src/Q10.java
+++ Java Loops/src/Q10.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Test {
+public class Q10 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
