15e5afaf7339b7842846140816230af9df01158a
Akarshan-upadhyay <121966381+Akarshan-upadhyay@users.noreply.github.com>
2023-01-06 16:13:59 +0530

    Add files via upload

---
+++ Java Loops/src/Q10.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Test {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        for(int i = 1; i * i <= n; i++){
+            System.out.print(i*i + " "); //only those bulbs will remain on which are perfect
+            //squares as perfect squares have odd number of divisors due to their square root being
+            //the extra one, whereas rest have even number of divisors
+        }
+    }
+}
